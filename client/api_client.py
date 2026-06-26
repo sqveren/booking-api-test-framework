17cd9c7aa8d450a6ce791094869730220e32f6bc
sqveren <sqveren@gmail.com>
2026-06-26 17:25:38 +0300

    add http methods

--- client/api_client.py
+++ client/api_client.py
@@ -1,1 +1,38 @@
+import requests
+from requests import Response
+
+
+class APIClient:
+
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.session = requests.Session()
+
+    def get(self, endpoint: str):
+        url = f"{self.base_url}/{endpoint}"
+        response = self.session.get(url)
+        return response
+
+    def post(self, endpoint: str, data: dict = None, **kwargs):
+        url = f"{self.base_url}/{endpoint}"
+        response = self.session.post(url, json=data, **kwargs)
+        return response
+
+    def put(self, endpoint: str, data: dict=None, **kwargs):
+        url = f"{self.base_url}/{endpoint}"
+        response = self.session.put(url, json=data, **kwargs)
+        return response
+
+    def patch(self, endpoint: str, data: dict=None, **kwargs):
+        url = f"{self.base_url}/{endpoint}"
+        response = self.session.patch(url, json=data, **kwargs)
+        return response
+
+    def delete(self, endpoint: str):
+        url = f"{self.base_url}/{endpoint}"
+        response = self.session.delete(url)
+        return response
+
+
+
 
