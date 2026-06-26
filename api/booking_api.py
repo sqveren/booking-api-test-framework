8b66ccb5c6de5e94664fe79d5339c4d1a568d00f
sqveren <sqveren@gmail.com>
2026-06-26 18:57:48 +0300

    add booking api methods

---
+++ api/booking_api.py
@@ -0,0 +1,24 @@
+from client.api_client import APIClient
+
+class BookingAPI:
+    def __init__(self, client):
+        self.client = client
+
+    def create_booking(self, data):
+        return self.client.post("/booking", data)
+
+    def get_booking(self, booking_id):
+        return self.client.get(f"/booking/{booking_id}")
+
+    def update_booking(self, booking_id, data):
+        return self.client.put(f"/booking/{booking_id}", data)
+
+    def delete_booking(self, booking_id):
+        return self.client.delete(f"/booking/{booking_id}")
+
+    def partial_update_booking(self,booking_id, data):
+        return self.client.patch(f"/booking/{booking_id}", data)
+
+    def get_all_bookings(self):
+        return self.client.get("/booking")
+
