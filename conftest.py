c8fdd0ffef3a92789f9d4875226af4f2364f4abd
sqveren <sqveren@gmail.com>
2026-06-29 14:24:41 +0300

    add models to conftest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from client.api_client import APIClient
 from api.booking_api import BookingAPI
+from models.booking_model import Booking, BookingDates
 
 @pytest.fixture(scope="session")
 def booking_api():
@@ -14,14 +15,14 @@ def auth_headers(booking_api):
 
 @pytest.fixture
 def booking_data():
-    return {
-        "firstname": "Jim",
-        "lastname": "Brown",
-        "totalprice": 111,
-        "depositpaid": True,
-        "bookingdates": {
-            "checkin": "2024-01-01",
-            "checkout": "2024-01-05"
-        },
-        "additionalneeds": "Breakfast"
-    }
+    return Booking(
+        firstname="Jim",
+        lastname="Brown",
+        totalprice=111,
+        depositpaid=True,
+        bookingdates=BookingDates(
+            checkin="2024-01-01",
+            checkout="2024-01-05"
+        ),
+        additionalneeds="Breakfast"
+    ).to_dict()
